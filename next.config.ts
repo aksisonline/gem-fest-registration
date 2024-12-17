4e226be93cfe98f7b8cd41d8e298fe96c4b4ab60
aksisonline <teamscientify2016@gmail.com>
2024-12-18 00:35:11 +0530

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // Disable type checking during build
+  },
 };
 
 export default nextConfig;
